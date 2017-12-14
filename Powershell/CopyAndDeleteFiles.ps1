9b5acbf8a1833b8156c19eab2f00a5d3c11ed562
freethewhat <matthew.c.sutton@gmail.com>
2017-12-14 15:43:32 -0700

    Added copy and delete powershell file

---
+++ Powershell/CopyAndDeleteFiles.ps1
@@ -0,0 +1,21 @@
+﻿$src = "C:\temp\testDirSrc"
+$dst = "C:\temp\testDirdst"
+$log = "C:\temp\copy.log"
+
+Set-Location $src
+
+ForEach($i in ls) {
+    $date = Get-Date
+    $success = copy-item $i.name $dst -PassThru -ErrorAction SilentlyContinue
+
+    if($success){
+        remove-item $i.Name
+        $success_log = $date.ToString() + " - Copy Successful: " + $src + "\" + $i.Name + " ~> " + $dst + "\" + $i.Name
+  
+        echo $success_log >> $log
+    } else {
+        $failure_log = $date.ToString() + " - Copy Failure: " + $src + "\" + $i.Name
+
+        echo $failure_log >> $log
+    }
+}
