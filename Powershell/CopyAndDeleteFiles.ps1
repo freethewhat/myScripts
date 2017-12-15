26dd708a938d13559a57e79da8efd7026ac26f16
freethewhat <matthew.c.sutton@gmail.com>
2017-12-15 08:38:59 -0700

    Update FIle Copy
    Changed the log to write to the application log.

--- Powershell/CopyAndDeleteFiles.ps1
+++ Powershell/CopyAndDeleteFiles.ps1
@@ -1,8 +1,16 @@
-﻿$src = "C:\temp\testDirSrc"
+﻿<#
+    Copies a file and verified the copy was successful. If it was succesful it will delete the old file and log successfully 
+    in the application log. If the copy does not complete successfully the file will not be deleted and will log a failure in
+    the application log.
+#>
+
+$src = "C:\temp\testDirSrc"
 $dst = "C:\temp\testDirdst"
-$log = "C:\temp\copy.log"
+$logSource = "FileCopy"
 
 Set-Location $src
+
+New-EventLog -Source $logSource -LogName Application
 
 ForEach($i in ls) {
     $date = Get-Date
@@ -10,12 +18,13 @@ ForEach($i in ls) {
 
     if($success){
         remove-item $i.Name
+
         $success_log = $date.ToString() + " - Copy Successful: " + $src + "\" + $i.Name + " ~> " + $dst + "\" + $i.Name
-  
-        echo $success_log >> $log
+        
+        Write-EventLog -LogName Application -Source $logSource -EventID "0" -EntryType Information -Message $success_log
     } else {
         $failure_log = $date.ToString() + " - Copy Failure: " + $src + "\" + $i.Name
 
-        echo $failure_log >> $log
+        Write-EventLog -LogName Application -Source $logSource -EventID "1" -EntryType Error -Message $failure_log
     }
 }
