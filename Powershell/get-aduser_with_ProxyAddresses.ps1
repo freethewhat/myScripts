a3ad3a02abf1f565612384e4adaa7a500e3d1c40
freethewhat <matthew.c.sutton@gmail.com>
2018-04-05 15:25:23 -0600

    Added Script to export users with proxyaddresses

---
+++ Powershell/get-aduser_with_ProxyAddresses.ps1
@@ -0,0 +1,4 @@
+import-module activedirectory
+
+get-aduser -filter * -Properties EmailAddress, ProxyAddresses | where {$_.ProxyAddresses -ne $null} | select-object GivenName, name, SamAccountName, EmailAddress, @{Name=’proxyAddresses’;Expression={[string]::join(“;”, ($_.proxyAddresses))}} | export-csv C:\temp\Users.csv
+write-host "Users Exported to C:\temp\users.csv"
